4fa1f20fb8b1878eb4482ea9daca51f47e52a26d
ggsdduyyds <ggsdduyyds@foxmail.com>
2022-09-26 21:18:16 +0800

    modify P1046.c

--- src/P1046.c
+++ src/P1046.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
 int main() {
-	int apples[10];
-	int h, count;
-	scanf("%d %d %d %d %d %d %d %d %d %d", &apples[0], &apples[1], &apples[2], &apples[3], &apples[4], &apples[5], &apples[6], &apples[7], &apples[8], &apples[9]);
-	scanf("%d", &h);
+	int a[10];
+	int h, c = 0;
+	scanf("%d %d %d %d %d %d %d %d %d %d", &a[0], &a[1], &a[2], &a[3], &a[4], &a[5], &a[6], &a[7], &a[8], &a[9]);
 	h = h + 30;
 	for (int i=0; i<10; i++) {
-		if (h >= apples[i]) {
-			count++;
+		if (h >= a[i]) {
+			c++;
 		}
 	}
-	printf("%d\n", count);
+	printf("%d", c);
 	return 0;
 }
